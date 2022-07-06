c08ec7ab2e70d870ee5a180b85c1c746ce23905b
Kelsi Lane <kelsilane91@gmail.com>
2022-07-06 16:55:28 -0500

    convert to styled components for button

--- react/course-goals-project/src/components/UI/Button/Button.tsx
+++ react/course-goals-project/src/components/UI/Button/Button.tsx
@@ -1,18 +1,31 @@
 import React, { ReactNode } from "react";
 
 import "./Button.css";
+import styled from "styled-components";
 
-type Props = {
-  type: "button" | "submit" | "reset" | undefined;
-  onClick?: () => void;
-  children: ReactNode;
-};
-const Button = ({ type, children, onClick }: Props) => {
-  return (
-    <button type={type} className="button" onClick={onClick}>
-      {children}
-    </button>
-  );
-};
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+  width: 100%;
+  @media (min-width: 768px) {
+    width: auto;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`;
 
 export default Button;
