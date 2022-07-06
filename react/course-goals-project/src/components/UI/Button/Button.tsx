9aab73b9c087e24ccc898514773e5d17693e8f0a
Kelsi Lane <kelsilane91@gmail.com>
2022-07-06 17:05:54 -0500

    include css module

--- react/course-goals-project/src/components/UI/Button/Button.tsx
+++ react/course-goals-project/src/components/UI/Button/Button.tsx
@@ -1,31 +1,43 @@
 import React, { ReactNode } from "react";
 
-import "./Button.css";
 import styled from "styled-components";
+import styles from "./Button.module.css";
+type Props = {
+  type: "button" | "submit" | "reset" | undefined;
+  onClick?: () => void;
+  children: ReactNode;
+};
 
-const Button = styled.button`
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
-  width: 100%;
-  @media (min-width: 768px) {
-    width: auto;
-  }
+// const Button = styled.button`
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
+//   width: 100%;
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
 
-  &:focus {
-    outline: none;
-  }
+//   &:focus {
+//     outline: none;
+//   }
 
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
+const Button = ({ type, children, onClick }: Props) => {
+  return (
+    <button type={type} className={styles.button} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
 
 export default Button;
