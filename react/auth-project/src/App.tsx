44e5104cd1e9279b4a6eef36ac667f70826fba8c
Kelsi Lane <kelsi.lane@pearlhealth.com>
2022-07-15 16:23:06 -0500

    clean up unused import

--- react/auth-project/src/App.tsx
+++ react/auth-project/src/App.tsx
@@ -3,7 +3,7 @@ import React, { useContext } from "react";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
-import AuthContext, { AuthContextProvider } from "./context/auth-context";
+import AuthContext from "./context/auth-context";
 
 function App() {
   const { isLoggedIn } = useContext(AuthContext);
