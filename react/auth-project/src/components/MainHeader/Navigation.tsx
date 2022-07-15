3d786f90c7a7ab7d678b2e92fd6f6a5b1ed3c9bd
Kelsi Lane <kelsi.lane@pearlhealth.com>
2022-07-15 15:58:11 -0500

    convert consumer to hook

--- react/auth-project/src/components/MainHeader/Navigation.tsx
+++ react/auth-project/src/components/MainHeader/Navigation.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import AuthContext from "../../context/auth-context";
 
 import classes from "./Navigation.module.css";
@@ -7,32 +7,27 @@ type Props = {
   onLogout: () => void;
 };
 const Navigation = ({ onLogout }: Props) => {
+  const { isLoggedIn } = useContext(AuthContext);
   return (
-    <AuthContext.Consumer>
-      {({ isLoggedIn }) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {isLoggedIn && (
-                <li>
-                  <button onClick={onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {isLoggedIn && (
+          <li>
+            <button onClick={onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
